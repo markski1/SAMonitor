71730e10728d96534e274a8abe0056462d2d3a34
Juan G <me@markski.ar>
2023-10-11 22:20:44 -0300

    Remove unused logger

--- API/Controllers/ApiController.cs
+++ API/Controllers/ApiController.cs
@@ -10,13 +10,6 @@ namespace SAMonitor.Controllers;
 [Route("api")]
 public class ApiController : ControllerBase
 {
-    private readonly ILogger<ApiController> _logger;
-
-    public ApiController(ILogger<ApiController> logger)
-    {
-        _logger = logger;
-    }
-
     [HttpGet("CheckAlive")]
     public string CheckAlive()
     {
