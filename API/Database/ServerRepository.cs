aa2f32d7a1b7e18e0adf629ffb5d8a22ffa7c075
Juan G. <me@markski.ar>
2024-05-21 20:46:00 -0300

    Better, thread-safe mysql connection pool

--- API/Database/ServerRepository.cs
+++ API/Database/ServerRepository.cs
@@ -14,10 +14,11 @@ namespace SAMonitor.Database
     }
     public class ServerRepository
     {
-        private static readonly List<MySqlConnection> _databasePool = [];
+        private static readonly List<MySqlConnection> _availableConnections = [];
+
         private static readonly object _lock = new object();
 
-        private static MySqlConnection DbConnection()
+        private static MySqlConnection GetConnection()
         {
             lock (_lock)
             {
@@ -25,46 +26,64 @@ namespace SAMonitor.Database
 
                 MySqlConnection? connection = null;
 
-                foreach (MySqlConnection conn in _databasePool)
+                // Get an open connection from the available connection pool.
+                foreach (MySqlConnection conn in _availableConnections)
                 {
                     if (conn.State == System.Data.ConnectionState.Open)
                     {
                         connection = conn;
                         break;
                     }
+
+                    // Broken or closed connections are added to a removal list.
+                    // Because _availableConnections cannot be mutated while being iterated.
                     if (conn.State == System.Data.ConnectionState.Closed || conn.State == System.Data.ConnectionState.Broken)
                     {
                         removal.Add(conn);
                     }
                 }
 
+                // Remove all connections in the removal list, if any.
                 foreach (MySqlConnection conn in removal)
                 {
-                    _databasePool.Remove(conn);
+                    _availableConnections.Remove(conn);
                     conn.Dispose();
                 }
 
+                // If a connection was found, remove it from the available list and return it.
                 if (connection != null)
                 {
+                    _availableConnections.Remove(connection);
                     return connection;
                 }
 
+                // Otherwise, create a new connection.
                 var newConn = new MySqlConnection(MySql.ConnectionString);
-                _databasePool.Add(newConn);
 
                 return newConn;
             }
         }
 
+        private static void ReturnConnection(MySqlConnection connection)
+        {
+            lock (_lock)
+            {
+                _availableConnections.Add(connection);
+            }
+        }
+
         public async Task<List<Server>> GetAllServersAsync()
         {
-            var db = DbConnection();
+            var db = GetConnection();
 
             const string sql = @"SELECT id, ip_addr, name, last_updated, is_open_mp, lag_comp, map_name, gamemode, players_online, max_players, website, version, language, sampcac, sponsor_until FROM servers";
 
             try
             {
-                return (await db.QueryAsync<Server>(sql)).ToList();
+                var result = (await db.QueryAsync<Server>(sql)).ToList();
+                
+                ReturnConnection(db);
+                return result;
             }
             catch (Exception ex)
             {
@@ -75,13 +94,16 @@ namespace SAMonitor.Database
 
         public async Task<int> GetServerId(string ipAddr)
         {
-            var db = DbConnection();
+            var db = GetConnection();
 
             var sql = @"SELECT id FROM servers WHERE ip_addr=@IpAddr";
 
             try
             {
-                return (await db.QueryAsync<int>(sql, new { IpAddr = ipAddr })).Single();
+                var result = (await db.QueryAsync<int>(sql, new { IpAddr = ipAddr })).Single();
+                
+                ReturnConnection(db);
+                return result;
             }
             catch (Exception ex)
             {
@@ -92,14 +114,14 @@ namespace SAMonitor.Database
 
         public async Task<bool> InsertServer(Server server)
         {
-            var db = DbConnection();
+            var db = GetConnection();
 
             string sql = @"INSERT INTO servers (ip_addr, name, last_updated, is_open_mp, lag_comp, map_name, gamemode, players_online, max_players, website, version, language, sampcac)
                         VALUES(@IpAddr, @Name, @LastUpdated, @IsOpenMp, @LagComp, @MapName, @GameMode, @PlayersOnline, @MaxPlayers, @Website, @Version, @Language, @SampCac)";
 
             try
             {
-                return (await db.ExecuteAsync(sql, new
+                var result = (await db.ExecuteAsync(sql, new
                 {
                     server.IpAddr,
                     server.Name,
@@ -115,6 +137,9 @@ namespace SAMonitor.Database
                     server.Language,
                     server.SampCac
                 })) > 0;
+
+                ReturnConnection(db);
+                return result;
             }
             catch (Exception ex)
             {
@@ -125,16 +150,19 @@ namespace SAMonitor.Database
 
         public async Task<int> InsertServerMetrics(int server_id, int player_amount)
         {
-            var db = DbConnection();
+            var db = GetConnection();
 
             string sql = @"INSERT INTO metrics_server (server_id, players) VALUES (@server_id, @player_amount)";
 
-            return await db.ExecuteAsync(sql, new { server_id, player_amount });
+            var result = await db.ExecuteAsync(sql, new { server_id, player_amount });
+            
+            ReturnConnection(db);
+            return result;
         }
 
         public async Task<bool> UpdateServer(Server server)
         {
-            var db = DbConnection();
+            var db = GetConnection();
 
             var sql = @"UPDATE servers
                         SET name=@Name, last_updated=@LastUpdated, is_open_mp=@IsOpenMp, lag_comp=@LagComp, map_name=@MapName, gamemode=@GameMode, players_online=@PlayersOnline, max_players=@MaxPlayers, website=@Website, version=@Version, language=@Language, sampcac=@SampCac
@@ -168,18 +196,23 @@ namespace SAMonitor.Database
 
             // then add a metric entry. ONLY IF IN PRODUCTION.
 
-            if (Global.IsDevelopment) return success;
+            if (Global.IsDevelopment)
+            {
+                ReturnConnection(db);
+                return success;
+            }
             
             sql = @"INSERT INTO metrics_server (server_id, players) VALUES (@Id, @PlayersOnline)";
 
             await db.ExecuteAsync(sql, new { server.Id, server.PlayersOnline });
 
+            ReturnConnection(db);
             return success;
         }
 
         public async Task<List<ServerMetrics>> GetServerMetrics(int id, DateTime requestTime, int include_misses = 0)
         {
-            var db = DbConnection();
+            var db = GetConnection();
 
             string sql;
 
@@ -192,7 +225,10 @@ namespace SAMonitor.Database
                 sql = @"SELECT players, time FROM metrics_server WHERE time > @requestTime AND server_id = @id AND players >= 0 ORDER BY time DESC";
             }
 
-            return (await db.QueryAsync<ServerMetrics>(sql, new { requestTime, id })).ToList();
+            var result = (await db.QueryAsync<ServerMetrics>(sql, new { requestTime, id })).ToList();
+
+            ReturnConnection(db);
+            return result;
         }
     }
 }
