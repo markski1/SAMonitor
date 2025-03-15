e1b25ce8c11aae55df3682ee8d13b7122a91ee62
Juan G. <me@markski.ar>
2025-03-15 05:14:07 -0300

    Fix stats models in API

--- API/Data/SmallObjects.cs
+++ API/Data/SmallObjects.cs
@@ -167,8 +167,14 @@ public class GlobalStats(int playersOnline, int serversTracked, int serversInhab
 
 public class CategoryStats
 {
-    private int Amount { get; set; } = 0;
-    private int Players { get; set; } = 0;
+    public int Amount { get; set; }
+    public int Players { get; set; }
+
+    public CategoryStats()
+    {
+        Amount = 0;
+        Players = 0;
+    }
 
     public void Add(Server server)
     {
@@ -179,25 +185,50 @@ public class CategoryStats
 
 public class LanguageStats
 {
-    public CategoryStats Spanish { get; set; } = new();
-    public CategoryStats Russian { get; set; } = new();
-    public CategoryStats English { get; set; } = new();
-    public CategoryStats Romanian { get; set; } = new();
-    public CategoryStats Portuguese { get; set; } = new();
-    public CategoryStats Asia { get; set; } = new();
-    public CategoryStats EastEuro { get; set; } = new();
-    public CategoryStats WestEuro { get; set; } = new();
-    public CategoryStats Other { get; set; } = new();
+    public CategoryStats Spanish { get; set; }
+    public CategoryStats Russian { get; set; }
+    public CategoryStats English { get; set; }
+    public CategoryStats Romanian { get; set; }
+    public CategoryStats Portuguese { get; set; }
+    public CategoryStats Asia { get; set; }
+    public CategoryStats EastEuro { get; set; }
+    public CategoryStats WestEuro { get; set; }
+    public CategoryStats Other { get; set; }
+
+    public LanguageStats()
+    {
+        Spanish = new();
+        Russian = new();
+        English = new();
+        Romanian = new();
+        Portuguese = new();
+        Asia = new();
+        EastEuro = new();
+        WestEuro = new();
+        Other = new();
+    }
 }
 
 public class GamemodeStats
 {
-    public CategoryStats Deathmatch { get; set; } = new();
-    public CategoryStats Roleplay { get; set; } = new();
-    public CategoryStats RaceStunt { get; set; } = new();
-    public CategoryStats Cnr { get; set; } = new();
-    public CategoryStats FreeRoam { get; set; } = new();
-    public CategoryStats Survival { get; set; } = new();
-    public CategoryStats VehSim { get; set; } = new();
-    public CategoryStats Other { get; set; } = new();
+    public CategoryStats Deathmatch { get; set; }
+    public CategoryStats Roleplay { get; set; }
+    public CategoryStats RaceStunt { get; set; }
+    public CategoryStats Cnr { get; set; }
+    public CategoryStats FreeRoam { get; set; }
+    public CategoryStats Survival { get; set; }
+    public CategoryStats VehSim { get; set; }
+    public CategoryStats Other { get; set; }
+
+    public GamemodeStats()
+    {
+        Deathmatch = new();
+        Roleplay = new();
+        RaceStunt = new();
+        Cnr = new();
+        FreeRoam = new();
+        Survival = new();
+        VehSim = new();
+        Other = new();
+    }
 }
