46b6f321f85251257774c2c2313f1489e89acc23
Juan G. <me@markski.ar>
2025-05-25 01:32:08 -0300

    Sane threadcount thresholds

--- API/Program.cs
+++ API/Program.cs
@@ -1,26 +1,26 @@
 using SAMonitor.Data;
 using SAMonitor.Utils;
 
-if (MySql.MySqlSetup())
-{
-    Console.WriteLine("Loading servers.");
-    await ServerManager.LoadServers();
-
-    Console.WriteLine("Loading statistics.");
-    StatsManager.LoadStats();
-
-    Console.WriteLine("Initializing server updater.");
-    ServerUpdater.Initialize();
-
-    WebServer.Initialize(args);
-
-    // This is a maximum. Yes, it is excessive, but I'm trying to live-diagnose
-    // what I suspect to be thread starvation. Cannot reproduce in development.
-    ThreadPool.SetMaxThreads(5000, 1000);
-
-    await Task.Delay(-1);
-}
-else
+if (!MySql.MySqlSetup())
 {
     Console.WriteLine("Could not generate ConnectionString for MySQL.\nExiting.");
+    return 1;
 }
+
+Console.WriteLine("Loading servers.");
+await ServerManager.LoadServers();
+
+Console.WriteLine("Loading statistics.");
+StatsManager.LoadStats();
+
+Console.WriteLine("Initializing server updater.");
+ServerUpdater.Initialize();
+
+WebServer.Initialize(args);
+
+ThreadPool.SetMinThreads(64, 32);
+ThreadPool.SetMaxThreads(256, 128);
+
+await Task.Delay(-1);
+
+return 0;
